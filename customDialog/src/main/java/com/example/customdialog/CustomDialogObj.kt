7b44248cc091e6ef29ddf6a7cc9b393ee4e610b1
Anand <ananda@getmyparking.com>
2021-10-01 20:15:32 +0530

    initial commit

---
+++ customDialog/src/main/java/com/example/customdialog/CustomDialogObj.kt
@@ -0,0 +1,23 @@
+package com.example.customdialog
+
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+
+object CustomDialogObj {
+    fun showDialog(
+        mContext: Context,
+        title: String,
+        message: String,
+        onPositiveButtonCallBack: () -> Unit
+    ) {
+        AlertDialog.Builder(mContext)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("dismiss", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    onPositiveButtonCallBack.invoke()
+                }
+            }).show()
+    }
+}
