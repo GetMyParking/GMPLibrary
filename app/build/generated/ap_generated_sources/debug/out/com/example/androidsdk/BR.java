dbfe7f65ea072e6279feaec00729fe6bd82f0265
uttamrabha <uttam@getmyparking.com>
2021-10-13 13:26:09 +0530

    intial commit

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/androidsdk/BR.java
@@ -0,0 +1,5 @@
+package com.example.androidsdk;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/androidsdk/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.androidsdk;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
